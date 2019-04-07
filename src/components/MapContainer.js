d0a1f95e072d61892429b9f9caf29d20f0ef24d9
Chris <Chris@Justins-MacBook-Air.local>
2019-04-07 16:09:25 -0400

    added two pins

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 
 const mapStyles = {
-  width: '90%',
-  height: '80%'
+  width: "50%",
+  height: "40%"
 };
 
 export class MapContainer extends Component {
-    state = {
-        showingInfoWindow: false,  //Hides or the shows the infoWindow
-        activeMarker: {},          //Shows the active marker upon click
-        selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
-      };
-    onMarkerClick = (props, marker, e) =>
-        this.setState({
-        selectedPlace: props,
-        activeMarker: marker,
-        showingInfoWindow: true
-     });
+  state = {
+    showingInfoWindow: false, //Hides or the shows the infoWindow
+    activeMarker: {}, //Shows the active marker upon click
+    selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
-    onClose = props => {
-        if (this.state.showingInfoWindow) {
-        this.setState({
-            showingInfoWindow: false,
-            activeMarker: null
-        });
-        }
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
   };
   render() {
     return (
@@ -34,17 +34,19 @@ export class MapContainer extends Component {
         zoom={14}
         style={mapStyles}
         initialCenter={{
-         lat: 43.6761256,
-         lng: -79.4097139
+          lat: 43.6761256,
+          lng: -79.4097139
         }}
       >
-      <Marker
-          onClick={this.onMarkerClick}
-          name={'Kenyatta International Convention Centre'}
+        <Marker
+          name={"where chris"}
+          title={"The marker`s title will appear as a tooltip."}
+          position={{ lat: 43.676041, lng: -79.403844 }}
         />
         <Marker
-          onClick={this.onMarkerClick}
-          name={''}
+          name={"where chris"}
+          title={"The marker`s title will appear as a tooltip."}
+          position={{ lat: 43.676326, lng: -79.41051 }}
         />
         <InfoWindow
           marker={this.state.activeMarker}
@@ -56,11 +58,10 @@ export class MapContainer extends Component {
           </div>
         </InfoWindow>
       </Map>
-      
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBqLt0dATSX7fhlrC3Ka-T2jalVrPIKyeQ'
+  apiKey: "AIzaSyBqLt0dATSX7fhlrC3Ka-T2jalVrPIKyeQ"
 })(MapContainer);
