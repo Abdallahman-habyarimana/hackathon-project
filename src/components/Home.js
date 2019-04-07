2a4079a7050708191b226b10f7e07b875f4a5284
Renata Moura <101096098@georgebrown.ca>
2019-04-07 17:28:05 -0400

    footer / i have to see the router

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
         }
     }
     handleClickOpen = () => {
-        this.setState({ open: true });n
+        this.setState({ open: true });
     };
         
     handleClose = () => {
